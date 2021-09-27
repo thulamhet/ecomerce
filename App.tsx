73cc3a253e37f18b353d319d49909843fdf339de
Nguyen Cong Thu <congthu137@gmail.com>
2021-09-27 16:12:46 +0700

    set up prj

---
+++ App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { SafeAreaView, StyleSheet, Text } from "react-native"
+
+const App = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>Ahahahaha</Text>
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, 
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+})
+
+export default App;
