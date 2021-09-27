7e40a5daa24e4859ba80787003bf7c80352f644d
Nguyen Cong Thu <congthu137@gmail.com>
2021-09-27 22:16:10 +0700

    up folder

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { SafeAreaView, StyleSheet, Text } from "react-native"
 const App = () => {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Ahahahaha</Text>
+      <Text>Ahahah1aha</Text>
     </SafeAreaView>
   )
 }
